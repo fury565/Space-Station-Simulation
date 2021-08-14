bb8055ef8150f40afdbe8e45393cd302cadf42ac
Bruno Licht <legotoster@gmail.com>
2021-08-14 21:18:00 +0200

    Incomplete
    need work

---
+++ experimental/JustSomeRandomRPGMechanics/JustSomeRandomRPGMechanics/Structure.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JustSomeRandomRPGMechanics
+{
+    class Structure
+    {
+        public List<Tile> designComponents { get; protected set; }
+        public List<Distance> componentLocation { get; protected set; }
+        public int PosX { get; protected set; }
+        public int PosY { get; protected set; }
+        
+        public Structure(int posX,int posY)
+        {
+            designComponents = new List<Tile>();
+            componentLocation = new List<Distance>();
+            PosX = posX;
+            PosY = posY;
+        }
+        public void AddComponent(Tile component,int x,int y)
+        {
+            designComponents.Add(component);
+            componentLocation.Add(new Distance(x, y));
+        }
+        public void RemoveComponent(int x,int y)
+        {
+
+            int counter = ReturnIndexOfComponentAtLocation(x,y);
+            designComponents[counter].Destroy();
+            if(designComponents[counter].GetTileDetails().Name=="open space")
+            {
+                componentLocation.Remove(componentLocation[counter]);
+                designComponents.Remove(designComponents[counter]);
+            }
+            
+        }
+        public int ReturnIndexOfComponentAtLocation(int x,int y)
+        {
+            int counter = 0;
+            foreach (Distance location in componentLocation)
+            {
+                if (location.X == x && location.Y == y)
+                    break;
+                counter++;
+            }
+            return counter;
+        }
+		public void ChooseComponent(){
+			int counter=0;
+            List<Tile> activable = new List<Tile>();
+            foreach(Tile component in designComponents)
+            {
+                if (component.GetTileDetails().Interactable &&component.GetTileDetails().Name!="console")
+                    activable.Add(component);
+            }
+			while(true){
+				Display.DisplayStructureOptions(activable,counter);
+				ConsoleKeyInfo pressed=Console.ReadKey(true);
+                if (pressed.Key == ConsoleKey.Enter)
+                {
+                    ActivateComponent(activable, counter);
+                    break;
+                }
+				else if(pressed.Key==ConsoleKey.DownArrow)
+					counter++;
+				else if(pressed.Key==ConsoleKey.UpArrow)
+					counter--;
+			}
+			
+			
+		}
+        public virtual void ActivateComponent(List<Tile> activables,int index)
+        {
+            if(activables[index].GetTileDetails().Effect=="FireLaser"){
+				//implement laser firing
+			}
+        }
+    }
+}
---
+++ experimental/JustSomeRandomRPGMechanics/JustSomeRandomRPGMechanics/Vehicle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JustSomeRandomRPGMechanics
+{
+    class Vehicle:Structure
+    {
+		public int Rotation{get;private set;}
+        public Vehicle(int posx,int posy) : base(posx, posy)
+        {
+
+        }
+        public void Move(int xDistance,int yDistance)
+        {
+            base.PosX += xDistance;
+            base.PosY += yDistance;
+            foreach(Distance Location in componentLocation)
+            {
+                Location.Change(Location.X+xDistance,Location.Y+yDistance);
+            }
+        }
+		public void RotateRight(){
+			Rotation+=90;
+			if(Rotation==360)
+				Rotation=0;
+		}
+		public void RotateLeft(){
+			Rotation-=90;
+			if(Rotation<0)
+				Rotation=270;
+		}
+		/*public override void ActivateComponent(int x,int y)
+        {
+            int index = ReturnIndexOfComponentAtLocation(x, y);
+            if(designComponents[index].GetTileDetails().Effect=="FireLaser"){
+				//implement laser firing
+			}
+        }*/
+    }
+}
